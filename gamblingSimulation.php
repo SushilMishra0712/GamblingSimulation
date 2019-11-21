ec5fd5515a92ac1a1b2fce6f155aee42f3133692
smitagaikwad25 <gaikwadsmita021@gmail.com>
2019-11-21 12:16:50 +0530

    refactored the variables of the previous program

--- gamblingSimulation.php
+++ gamblingSimulation.php
@@ -2,9 +2,13 @@
 
 $stake=100;
 $bet=1;
+$counter=1;
 echo "Press Enter to gamble";
 fscanf(STDIN,"%s");
-while($stake>0)
+$min_stake_percentage = $stake/2;
+$max_stake_percentage = $stake+($stake/2);
+
+while($counter>0)
 {
    $random = rand(0,1);
    if($random==1)
@@ -15,11 +19,11 @@ while($stake>0)
    {
       $stake-=$bet;
    }
-   if($stake==50 || $stake==150)
+   if($stake==$min_stake_percentage || $stake==$max_stake_percentage)
    {
       echo "You had resigned for the day\n";
       echo "Your stake amount at end of the day is:".$stake."\n";
-      exit(0);
+      $counter--;
    }
 }
 
