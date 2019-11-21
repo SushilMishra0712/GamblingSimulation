5b5f366d62061a4c3fe1c9271851aae1d8fa693c
smitagaikwad25 <gaikwadsmita021@gmail.com>
2019-11-21 11:57:55 +0530

    gambler resign for the day after win or lost 50% of stake

--- gamblingSimulation.php
+++ gamblingSimulation.php
@@ -4,16 +4,23 @@ $stake=100;
 $bet=1;
 echo "Press Enter to gamble";
 fscanf(STDIN,"%s");
-$random = rand(0,1);
-if($random==1)
+while($stake>0)
 {
-    $stake+=$bet;
-    echo "You win bet\n";
-}
-else
-{
-    $stake-=$bet;
-    echo "You loose bet\n";
+   $random = rand(0,1);
+   if($random==1)
+   {
+      $stake+=$bet;
+   }
+   else
+   {
+      $stake-=$bet;
+   }
+   if($stake==50 || $stake==150)
+   {
+      echo "You had resigned for the day\n";
+      echo "Your stake amount at end of the day is:".$stake."\n";
+      exit(0);
+   }
 }
 
 ?>
