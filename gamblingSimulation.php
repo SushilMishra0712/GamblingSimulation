50c217a9f491259132b791c6dbc422b3e01c31f6
Sushil <SushilMishra0712>
2019-11-21 10:35:31 +0530

    gambler made bet to either win or loose

--- gamblingSimulation.php
+++ gamblingSimulation.php
@@ -2,5 +2,18 @@
 
 $stake=100;
 $bet=1;
+echo "Press Enter to gamble";
+fscanf(STDIN,"%s");
+$random = rand(0,1);
+if($random==1)
+{
+    $stake+=$bet;
+    echo "You win bet\n";
+}
+else
+{
+    $stake-=$bet;
+    echo "You loose bet\n";
+}
 
 ?>
